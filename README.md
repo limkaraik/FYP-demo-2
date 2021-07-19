1f7488c9c9fe6a79b8ab73856572f8d0de25b8c4
Kar Aik Lim <limkaraik@hotmail.com>
2021-07-19 20:38:59 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# fyp_demo_python
+## How to create exe file
+#### step 1: pip install pyinstaller
+#### step 2: using cmd, go to the directory where the python file is at
+#### step 3: type this command: pyinstaller demo.py --onefile --noconsole
+#### step 4: after pyinstaller finishes doing its thing, go to dist folder and drag the demo.exe out
+#### do note that all the other dependencies such as images and other python file needs to be in the same directory as the exe file
---
+++ demo.py
@@ -0,0 +1,114 @@
+import pygame
+import time
+pygame.init()
+
+from graph import Graph
+from misc import Misc
+from algo import Algo
+from data import DATA
+
+WINDOW_SIZE = (1200,800)
+BACKGROUND = (255,255,255)
+
+
+#Create the screen
+screen = pygame.display.set_mode(WINDOW_SIZE)
+
+#title and icon
+pygame.display.set_caption("Demo")
+
+#misc functions
+misc = Misc(screen)
+
+
+def play():
+    #env
+    goals = [1,7,61,70]
+    theif = 31
+    defenders = [13,36,45]
+    limit=20
+    gameover = False
+
+    #init graph
+    graph = Graph(screen,theif,defenders,goals)
+
+    #init algo
+    algo = Algo(DATA)
+
+    #main loop
+    running = True
+
+    while running:
+        screen.fill(BACKGROUND)
+
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONUP:
+                #player select where to move for thief
+                turn, attacker_a =graph.check_move()
+                print(turn, attacker_a)
+                if turn:
+                    #police move base on algo here
+                    defender_a=algo.move(graph.thief,graph.police)
+                    #move police in graph
+                    graph.police_move(defender_a)
+
+                    limit-=1
+                #after police has moved etc....
+                graph.reset_move()
+            
+            elif event.type == pygame.QUIT:
+                running = False
+            
+        
+        #check who win
+        result = graph.checkWin()
+        if result ==1: #player win
+            misc.message_to_screen('Player wins!',(255,0,0),0,0,'large')
+            m= misc.button("Restart",850,320,150,150,(55,255,255),(0,255,0))
+            gameover = True
+            if m:
+                return
+        elif result ==2: #police win
+            #draw game boundary
+            pygame.draw.rect(screen,(0,0,0),(50,50,700,700),1)
+            #display graph
+            graph.display_lose()
+            #display turns left
+            misc.message_to_screen('You got caught! Player lose...',(0,0,255),330,-200,'small')
+            m1= misc.button("Restart",850,320,150,150,(55,255,255),(0,255,0))
+            gameover = True
+            if m1:
+                return
+        elif limit==0:
+            misc.message_to_screen('Time Out! Player Lose...',(0,255,0),0,0,'large')
+            m2= misc.button("Restart",500,500,150,150,(55,255,255),(0,255,0))
+            gameover = True
+            if m2:
+                return
+        else:
+            #display graph
+            graph.display()
+            #display turns left
+            misc.message_to_screen('Turns left: '+str(limit),(0,0,255),310,-200,'medium')
+            m= misc.button("Return",850,320,150,150,(55,255,255),(0,255,0))
+            if m:
+                return
+        pygame.display.update()
+
+def main():
+    #main loop
+    running = True
+    while running:
+        screen.fill(BACKGROUND)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        #select difficulty
+        misc.message_to_screen("Click Start to play!",(0,0,0),0,-150,'large')
+        m= misc.button("Start",500,350,150,150,(55,255,255),(0,255,0))
+        if m:
+            play()
+        pygame.display.update()
+
+main()
